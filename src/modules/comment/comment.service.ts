851bd9f086ff97811937daa71fd9e141e46515a2
S. D. Dikov <s.d.dikov@gmail.com>
2023-02-04 15:27:18 +0300

    save

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,6 +1,7 @@
 import {inject, injectable} from 'inversify';
 import {DocumentType, types} from '@typegoose/typegoose';
 import {CommentServiceInterface} from './comment-service.interface.js';
+import {LoggerInterface} from '../../common/logger/logger.interface.js';
 import {Component} from '../../types/component.types.js';
 import {CommentEntity} from './comment.entity.js';
 import CreateCommentDto from './dto/create-comment.dto.js';
@@ -8,24 +9,21 @@ import CreateCommentDto from './dto/create-comment.dto.js';
 @injectable()
 export default class CommentService implements CommentServiceInterface {
   constructor(
+    @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.CommentModel) private readonly commentModel: types.ModelType<CommentEntity>
   ) {}
 
   public async create(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>> {
-    const comment = await this.commentModel.create(dto)
-    comment.aggregate([
-      {
-        $lookup: {
-          from: 'comments',
-          let: { offerId: '$_id'},
-          pipeline: [
-            { $match: { $expr: { $eq: ['$$offerId', '$categories'] } } },
-            { $project: { _id: 1}}
-          ],
-          as: 'offers'
-        },
-      },
+    const comment = await this.commentModel.create(dto);
+
+    const rating = await this.commentModel.aggregate([
+      { $match: { offerId: dto.offerId } },
+      { $group : {_id: '$avdRating', avgAmount: {$avg: '$ratirng'}}},
+      { $merge: { into: 'offers', on: 'rating', whenMatched: 'replace', whenNotMatched: 'insert' } }
     ]);
+
+    this.logger.info(`rating: ${rating}`);
+
     return comment.populate('userId');
   }
 
